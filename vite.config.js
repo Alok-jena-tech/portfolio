2973bcbaf8697354b8041c31f87392cd0f809e3c
alok kumar <alokjena3896@gmial.com>
2025-08-15 16:10:56 +0530

    okk

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    react()
   ],
+   build: {
+    target: 'esnext',        // ensures latest JS syntax support
+    minify: 'esbuild',       // faster minification
+  },
+  optimizeDeps: {
+    include: ['react-toastify'], // pre-bundle react-toastify to avoid "use client" errors
+  },
+  resolve: {
+    alias: {
+      '@': '/src',           // optional, if you use '@' for src imports
+    },
+  },
 })
